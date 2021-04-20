cc682f633c042141383e5538810153f7ace410d1
Elijah <efroh10@gmail.com>
2021-04-20 11:06:37 -0700

    completed modularity and added fetch to integrate with api

--- donutShopHelpers.js
+++ donutShopHelpers.js
@@ -1,102 +1,148 @@
 function getInventory(){
-    console.log(masterInventory); 
-    useInterface();
+    fetch("https://donutshop-api.herokuapp.com/inventory?id=514", {
+  "method": "GET",
+})
+.then(response => response.json())
+.then(data => console.log(data.donuts))
+.catch(err => {
+  console.error(err);
+});
  }
  
  function getRevenue(){
-     alert(revenue);
-     useInterface();
+    fetch("https://donutshop-api.herokuapp.com/revenue?id=514", {
+        "method": "GET",
+      })
+      .then(response => response.json())
+      .then(data => console.log(data.revenue))
+      .catch(err => {
+        console.error(err);
+      });
  }
  
  function newDonutType(){
-     newType = prompt('Enter the type of your new donut.');
+     let newType = prompt('Enter the type of your new donut.');
      return newType;
  }
  
- function newDonutName(){
-     newName = prompt('Enter the name of your new donut.');
-     return newName;
- }
- 
  function newDonutPrice(){
-     newPrice = Number(prompt('Enter the price of your new donut.'));
+     let newPrice = Number(prompt('Enter the price of your new donut.'));
      return newPrice;
  }
  
- function newDonutInventory(){
-     newInventory = Number(prompt('Enter the inventory of your new donut.'));
-     return newInventory;
+ function newDonut(type, price){
+    fetch("https://donutshop-api.herokuapp.com/create-donut-type?id=514", {
+        "method": "POST",
+        "headers": {
+        "Content-Type": "application/json"
+        },
+        "body": JSON.stringify({'type': type, 'price': price})
+        })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => {
+    console.error(err);
+    });
  }
- 
- function newDonut(type, name, price, inventory){
-     masterInventory[type] = new Donut(name, price, inventory);
-     useInterface();
- }
- //this program assumes throughout that the user knows all donut types.
+
  function increaseType(){
-     increasedType = prompt('Enter the type of donut for which you\'d like to increase the inventory.');
+     let increasedType = prompt('Enter the type of donut for which you\'d like to increase the inventory.');
      return increasedType;  
  }
  
  function increaseNumber(){
-     increasedNumber = Number(prompt('Enter the number by which you\'d like to increase the inventory.'));
+     let increasedNumber = Number(prompt('Enter the number by which you\'d like to increase the inventory.'));
      return increasedNumber;
  }
  
  function increaseInventory(type, number){
-     masterInventory[type].inventory += number;
-     useInterface();
+    fetch("https://donutshop-api.herokuapp.com/add-donuts?id=514", {
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body":JSON.stringify({'type': type, 'count': number})
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => {
+        console.error(err);
+      });
  }
  
  function newOrderType(){
-     newOrderedType = prompt('Enter the type of donut you\'d like to order.');
+     let newOrderedType = prompt('Enter the type of donut you\'d like to order.');
      return newOrderedType;
  }
  
  function newOrderNumber(){
-     newOrderedNumber = Number(prompt('Enter the number of donuts you\'d like to order.'));
+     let newOrderedNumber = Number(prompt('Enter the number of donuts you\'d like to order.'));
      return newOrderedNumber;
  }
  
- function decreaseInventory(type, number){
-     masterInventory[type].inventory -= number;
-     
- }
- 
- function increaseRevenue(type, number){
-     revenue += masterInventory[type].price * number;
- }
- 
  function newOrder(type, number){
-     if (number > masterInventory[type].inventory){
-         number = masterInventory[type].inventory;
-     }
-     decreaseInventory(type, number);
-     increaseRevenue(type, number);
-     useInterface();
+    fetch("https://donutshop-api.herokuapp.com/place-order?id=514", {
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body": JSON.stringify({'type': type, 'count':number})
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => {
+        console.error(err);
+      });
  }
  
  function newPriceType(){
-     changedType = prompt('Enter the type of donut for which you\'d like to change the price.');
+     let changedType = prompt('Enter the type of donut for which you\'d like to change the price.');
      return changedType;
  }
  
  function newPrice(){
-     changedPrice = Number(prompt('Enter the new price.'));
+     let changedPrice = Number(prompt('Enter the new price.'));
      return changedPrice;
  }
  
  function changePrice(type, number){
-     masterInventory[type].price = number;
-     useInterface();
+    fetch("https://donutshop-api.herokuapp.com/edit-donut?id=514", {
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body": JSON.stringify({'type': type, 'price': number})
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => {
+        console.error(err);
+      });
  }
  
- function getRefund(){
-     refund = Number(prompt('Enter the amount of the refund.'));
-     return refund;
+ function getRefundType(){
+     let refundType = (prompt('Enter the type of donut to be refunded.'));
+     return refundType;
+ }
+
+ function getRefundNumber(){
+     let refundNumber = Number(prompt('Enter the number of donuts to be refunded.'));
+     return refundNumber;
  }
  
- function giveRefund(number){
-     revenue -= number;
-     useInterface();
+ function giveRefund(type, number){
+    fetch("https://donutshop-api.herokuapp.com/refund?id=514", {
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body": JSON.stringify({'type': type, 'count': number})
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => {
+        console.error(err);
+      });
  }
+
+ export {getInventory, getRevenue, newDonutType, newDonutPrice, newDonut, increaseType, increaseNumber, increaseInventory, newOrderType, newOrderNumber, newOrder, newPriceType, newPrice, changePrice, getRefundType, getRefundNumber, giveRefund};
--- useInterface.js
+++ useInterface.js
@@ -1,28 +1,42 @@
+
+import {getInventory, getRevenue, newDonutType, newDonutPrice, newDonut, increaseType, increaseNumber, increaseInventory, newOrderType, newOrderNumber, newOrder, newPriceType, newPrice, changePrice, getRefundType, getRefundNumber, giveRefund} from './donutShopHelpers.js';
+
+
+
 function useInterface(){
     let task = prompt('What is your bidding, my master? Enter the number corresponding to the task you\'d like to perform: 1): Print Inventory and Prices, 2): Print Total Revenue, 3): Create New Donut Type, 4): Update Inventory, 5): Place Order, 6): Change a Price, 7): Give Refund, 8): Quit Program', '1');
     switch (task) {
         case '1':
             getInventory();
+            useInterface();
             break;
         case '2':
             getRevenue();
+            useInterface();
             break;
         case '3':
-            newDonut(newDonutType(), newDonutName(), newDonutPrice(), newDonutInventory());
+            newDonut(newDonutType(), newDonutPrice());
+            useInterface();
             break;
         case '4':
             increaseInventory(increaseType(), increaseNumber());
+            useInterface();
             break;
         case '5':
             newOrder(newOrderType(), newOrderNumber());
+            useInterface();
             break;
         case '6':
             changePrice(newPriceType(), newPrice());
+            useInterface();
             break;
         case '7':
-            giveRefund(getRefund());
+            giveRefund(getRefundType(), getRefundNumber());
+            useInterface();
             break;
         case '8':
             break;
     }
 }
+
+useInterface();
